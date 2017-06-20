602af0cc6d6b26e3481f4ea51882b075f8599926
jilldela <jilldelosangeles@gmail.com>
2017-06-20 09:40:13 -0700

    Create vertex and edge classes. Create topological sort using Kahn's algorithm

---
+++ lib/topological_sort.rb
@@ -0,0 +1,28 @@
+require_relative 'graph'
+require 'byebug'
+
+# Implementing topological sort using both Khan's and Tarian's algorithms
+
+def topological_sort(vertices)
+  in_edge_counts = {}
+  sorted = []
+  queue = []
+
+  vertices.each do |vertex|
+    in_edge_counts[vertex] = vertex.in_edges.count
+    queue << vertex if vertex.in_edges.empty?
+  end
+
+  until queue.empty?
+    current = queue.shift
+    sorted << current
+
+    current.out_edges.each do |edge|
+      in_edge_counts[edge.to_vertex] -= 1
+      queue << edge.to_vertex if in_edge_counts[edge.to_vertex] == 0
+    end
+
+  end
+
+  sorted.length == 2 ? [] : sorted
+end
